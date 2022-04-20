509da7f6a96bbf318eed920bc2489c4bd811fe80
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-04-20 23:10:20 +0300

    К названию папки OldMods теперь прибавляется дата

--- install (windows).bat
+++ install (windows).bat
@@ -1,14 +1,15 @@
-@echo off
-cd %appdata%/.minecraft/mods
-md OldMods
-copy /y *.jar %appdata%\.minecraft\mods\OldMods\*.jar
-erase  %appdata%\.minecraft\mods\*.jar
-cd %~dp0
-cd mods
-copy /y *.jar %appdata%\.minecraft\mods\*.jar
-erase *.jar
-cd ..
-rd mods
-echo #Установка завершена) > "installation completed.md"
-erase *.gitignore
+@echo off  
+cd %appdata%/.minecraft/mods 
+set oldmodsfolder=OldMods%date%  
+md %oldmodsfolder% 
+copy /y *.jar "%appdata%\.minecraft\mods\%oldmodsfolder%\*.jar"  
+erase  %appdata%\.minecraft\mods\*.jar  
+cd %~dp0  
+cd mods  
+copy /y *.jar %appdata%\.minecraft\mods\*.jar  
+erase *.jar  
+cd ..  
+rd mods  
+echo #Установка завершена) > "installation completed.md"  
+erase *.gitignore 
 erase *.bat
