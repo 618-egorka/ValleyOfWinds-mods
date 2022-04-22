621220eb3b18fa8fde5b45d7adc178c313c5f62d
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-04-22 22:53:11 +0300

    +Старые моды сохраняются, +сообщение о установке

--- install (windows).bat
+++ install (windows).bat
@@ -1,17 +1,18 @@
 @echo off  
-cd %appdata%/.minecraft/mods 
-set oldmodsfolder=OldMods%date%  
-md %oldmodsfolder% 
-copy /y *.jar "%appdata%\.minecraft\mods\%oldmodsfolder%\*.jar"  
-erase  %appdata%\.minecraft\mods\*.jar  
-cd %~dp0  
-cd mods  
-copy /y *.jar %appdata%\.minecraft\mods\*.jar  
-erase *.jar  
-cd ..  
+cd %appdata%/.minecraft/mods   
+set oldmodsfolder=OldMods%date% 
+chcp 65001  
+md %oldmodsfolder%   
+copy *.jar "%appdata%\.minecraft\mods\%oldmodsfolder%\*.jar"  
+erase  %appdata%\.minecraft\mods\*.jar    
+cd %~dp0    
+cd mods    
+copy /y *.jar %appdata%\.minecraft\mods\*.jar    
+erase *.jar    
+cd ..    
 rd mods  
-msg * "Сборка установлена)" >nul  
-erase *.gitignore 
-erase thumbnail.png
-erase README.md
+msg * "Сборка установлена)" >nul    
+erase *.gitignore   
+erase thumbnail.png  
+erase README.md  
 erase *.bat
