e7bd77977812d20862929aba67a459086541c266
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-05-01 17:30:34 +0300

    Код обновлён с учётом запуска из oth/..

--- oth/install (macOS).sh
+++ oth/install (macOS).sh
@@ -8,12 +8,12 @@ mkdir oldMods
 cp *.jar "~/Library/Application Support/minecraft/mods/oldMods"
 rm  "~/Library/Application Support/minecraft/mods/"*.jar
 rm  "~/Library/Application Support/minecraft/mods/oldMods/"FE*.jar
-cd $fullpath/mods
+cd $fullpath
+cd ..
+cd mods
 cp *.jar "~/Library/Application Support/minecraft/mods/"     
 cd ..     
 rm -r mods 
-rm *.gitignore   
-rm thumbnail.png  
+rm -r oth
+rm *.gitignore  
 rm README.md
-rm *.bat
-rm *.sh
