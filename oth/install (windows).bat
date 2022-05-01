28a952901da1cd8c215f631db2029459b768b8e5
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-05-01 19:13:07 +0300

    Теперь может сам создавать помощник удаления

--- oth/install (windows).bat
+++ oth/install (windows).bat
@@ -1,23 +1,31 @@
-@REM @echo off   
-@REM cd %appdata%/.minecraft 
-@REM md mods 
-@REM cd mods    
-@REM chcp 65001   
-@REM md oldMods  
-@REM copy *.jar "%appdata%\.minecraft\mods\oldMods\*.jar"   
-@REM erase  %appdata%\.minecraft\mods\*.jar 
-@REM erase  %appdata%\.minecraft\mods\oldMods\FE*.jar    
-@REM cd %~dp0     
+@REM @echo off    
+@REM cd %appdata%/.minecraft  
+@REM md mods  
 @REM cd mods     
-@REM copy /y *.jar %appdata%\.minecraft\mods\*.jar     
-@REM erase *.jar     
-@REM cd ..     
-@REM rd mods
-@REM msg * "Сборка установлена)" >nul
-@REM erase *.gitignore    
-@REM erase thumbnail.png   
-@REM erase README.md   
-@REM erase *.sh 
-@REM erase *.bat
-@REM rd /s oth
-@REM erase *.exe
+@REM chcp 65001    
+@REM md oldMods   
+@REM copy *.jar "%appdata%\.minecraft\mods\oldMods\*.jar"    
+@REM erase  %appdata%\.minecraft\mods\*.jar  
+@REM erase  %appdata%\.minecraft\mods\oldMods\FE*.jar     
+@REM cd %~dp0      
+@REM cd mods      
+@REM copy /y *.jar %appdata%\.minecraft\mods\*.jar 
+@REM cd .. 
+@REM erase *.gitignore      
+@REM erase README.md    
+@REM erase *.bat 
+@REM rd /s /q mods 
+@REM rd /s /q oth 
+@REM set helpfile=%~dp0%\delHelp.bat 
+@REM ( 
+@REM echo @echo OFF 
+@REM echo @REM taskkill /f /t /im ^"install (windows^).exe^" 
+@REM echo erase /f /q ^"install (windows^).exe^" 
+@REM echo chcp 65001 
+@REM echo msg * ^"Сборка установлена^)^" 
+@REM echo :end 
+@REM echo erase /f /q ^"delHelp.bat^" 
+@REM )>"%helpfile%" 
+@REM cd %~dp0 
+@REM start /min delHelp.bat 
+@REM exit
