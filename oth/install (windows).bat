b04a415a5a3a988787baf8431e1ad4fb93cd5280
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-05-01 17:22:13 +0300

    create

---
+++ oth/install (windows).bat
@@ -0,0 +1,21 @@
+@REM @echo off   
+@REM cd %appdata%/.minecraft 
+@REM md mods 
+@REM cd mods    
+@REM chcp 65001   
+@REM md oldMods  
+@REM copy *.jar "%appdata%\.minecraft\mods\oldMods\*.jar"   
+@REM erase  %appdata%\.minecraft\mods\*.jar 
+@REM erase  %appdata%\.minecraft\mods\oldMods\FE*.jar    
+@REM cd %~dp0     
+@REM cd mods     
+@REM copy /y *.jar %appdata%\.minecraft\mods\*.jar     
+@REM erase *.jar     
+@REM cd ..     
+@REM rd mods   
+@REM msg * "Сборка установлена)" >nul     
+@REM erase *.gitignore    
+@REM erase thumbnail.png   
+@REM erase README.md   
+@REM erase *.sh 
+@REM erase *.bat
