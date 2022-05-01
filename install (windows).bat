72eb5a3764c1876f4b1a387f905479779dc5b594
Егорка <101581584+618-egorka@users.noreply.github.com>
2022-05-01 16:17:25 +0300

    Улучшенное копирование старых модов
    Теперь директория, в которую копируются старые моды, всегда называется oldMods. В неё копируются только старые моды игрока. Если придётся копировать файлы старой сборки при её обновлении, они не будут перемещены в oldMods, а будут удалены. Файлы сборки помечаются подписью FE перед названием мода

--- install (windows).bat
+++ install (windows).bat
@@ -1,21 +1,21 @@
-@echo off  
-cd %appdata%/.minecraft
-md mods
-cd mods   
-set oldmodsfolder=OldMods%date% 
-chcp 65001  
-md %oldmodsfolder%   
-copy *.jar "%appdata%\.minecraft\mods\%oldmodsfolder%\*.jar"  
-erase  %appdata%\.minecraft\mods\*.jar    
-cd %~dp0    
+@echo off   
+cd %appdata%/.minecraft 
+md mods 
 cd mods    
-copy /y *.jar %appdata%\.minecraft\mods\*.jar    
-erase *.jar    
-cd ..    
-rd mods  
-msg * "Сборка установлена)" >nul    
-erase *.gitignore   
-erase thumbnail.png  
-erase README.md  
-erase *.sh
+chcp 65001   
+md oldMods  
+copy *.jar "%appdata%\.minecraft\mods\oldMods\*.jar"   
+erase  %appdata%\.minecraft\mods\*.jar 
+erase  %appdata%\.minecraft\mods\oldMods\FE*.jar    
+cd %~dp0     
+cd mods     
+copy /y *.jar %appdata%\.minecraft\mods\*.jar     
+erase *.jar     
+cd ..     
+rd mods   
+msg * "Сборка установлена)" >nul     
+erase *.gitignore    
+erase thumbnail.png   
+erase README.md   
+erase *.sh 
 erase *.bat
